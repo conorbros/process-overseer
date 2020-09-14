ef6db7915a4f2a2204f7157c2547663132eabaa6
Conor <conorbrosnan10@gmail.com>
2020-09-14 12:18:10 +1000

    Make timer better for testing signals

--- test/timer.c
+++ test/timer.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define WAIT 1
 
+int term_wait;
+
+void termination_handler()
+{
+    printf("Received termination signal...exiting\n");
+    sleep(term_wait);
+    exit(SIGTERM);
+}
+
 int main(int argc, char *argv[])
 {
+    term_wait = atoi(argv[2]);
+    signal(SIGTERM, termination_handler);
     if (argc < 1)
     {
         printf("Waiting for  default: %d\n", WAIT);
